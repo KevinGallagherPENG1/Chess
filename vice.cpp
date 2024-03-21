5a53ff11a6380b8c0e795cb14bef73c5b7e6d5ac
Kevin <kevingallagher7274@gmail.com>
2024-03-20 22:44:27 -0700

    Added attack.cpp to see if a square is being attacked by any possible piece

--- vice.cpp
+++ vice.cpp
@@ -9,6 +9,31 @@
 #define FEN2 "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
 #define FEN3 "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
 #define FEN4 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
+#define TESTFENILLEGAL "8/3q4/8/8/4Q3/8/8/8 w - - 0 2 "
+
+//To test if squares are being attacked
+//Looks good
+/*
+void ShowSqAtBySide(const int side, const S_BOARD *pos){
+    int rank = 0;
+    int file = 0;
+    int sq = 0;
+
+    printf("\n\nSquares attacked by:%c\n", SideChar[side]);
+    for(rank = RANK_8; rank >= RANK_1; rank--){
+        for(file = FILE_A; file <= FILE_H; file++){
+            sq = FR2SQ(file, rank);
+
+            if(SqAttacked(sq, side, pos) == TRUE)
+                printf("X");
+            else 
+                printf("-");
+        }
+        printf("\n");
+    }
+    printf("\n\n");
+}
+*/
 
 int main(){
 
@@ -19,6 +44,21 @@ int main(){
     //These work! Testing FEN parsing
     S_BOARD board[1];
 
+
+    //Testing SqAtBy
+    //Looks good
+    /*
+    ParseFen(TESTFENILLEGAL, board);
+    PrintBoard(board);
+
+    printf("\n\nWhite Attacking:\n");
+    ShowSqAtBySide(WHITE, board);
+
+    printf("\n\nBlack Attacking:\n");
+    ShowSqAtBySide(BLACK, board);
+
+    */
+   
     /*
     //Checking checkBoard, it Works :)
     ParseFen(FEN4, board);
